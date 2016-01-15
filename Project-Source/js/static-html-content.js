a8f1af8d2d2e3c22770e910e27068a5911ee4544
Bappy Golder <bups.co@gmail.com>
2016-01-16 01:06:26 +1100

    refactored

--- Project-Source/js/static-html-content.js
+++ Project-Source/js/static-html-content.js
@@ -1,13 +1,12 @@
-// contains static HTML data for the app. These are data that are not dinamically updated.
+// contains static HTML data for the app. These are data that are not dinamically updated. use the TEMPLATE sections to create new data fields. 
 
-
-//content variables | template: var a = "";
+//content variables | TEMPLATE: var a = "";
 	var app_title = "oTask";
 	var add_button_text = "Add";
 	var empty_list_message = "Your list is empty. Add something new!";
 	//var clear_list_text = "Clear List"; //need to work it out still
 
-//add content to HTML DOM | template: $("#").append();
+//add content to HTML DOM | TEMPLATE: $("#").append();
 	$("title").append(app_title);
 	$("#add_task_button").append(add_button_text);
 	$("#empty_msg").append(empty_list_message);
