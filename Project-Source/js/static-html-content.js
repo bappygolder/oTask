2da18bbc2e9edbae6d8b430f6d8147d1d7cdee7b
Bappy Golder <bups.co@gmail.com>
2016-01-16 00:39:32 +1100

    Static content JS file added
    - We can put all the static HTML data in this file so that the HTML DOM
    elements are all less cluttered.
    - Secondly this also provides one single file for all the data to be
    placed.
    - We will need to go through all the other files and include all the
    static data to this file.

---
+++ Project-Source/js/static-html-content.js
@@ -0,0 +1,9 @@
+// contains static HTML data for the app. These are data that are not dinamically updated.
+
+
+//content variables
+var app_title = "oTask";
+
+//add content to HTML DOM
+$("title").append(app_title);
+
