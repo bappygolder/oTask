99db431951daa0b389662b7df003f82610c6b9b7
Bappy Golder <bups.co@gmail.com>
2016-01-16 01:04:37 +1100

    Content and code restructured
    Some of the HTML static content now are brought from static...content.js
    file and some code has been restructured for better readability.

--- Project-Source/js/static-html-content.js
+++ Project-Source/js/static-html-content.js
@@ -1,9 +1,15 @@
 // contains static HTML data for the app. These are data that are not dinamically updated.
 
 
-//content variables
-var app_title = "oTask";
+//content variables | template: var a = "";
+	var app_title = "oTask";
+	var add_button_text = "Add";
+	var empty_list_message = "Your list is empty. Add something new!";
+	//var clear_list_text = "Clear List"; //need to work it out still
 
-//add content to HTML DOM
-$("title").append(app_title);
+//add content to HTML DOM | template: $("#").append();
+	$("title").append(app_title);
+	$("#add_task_button").append(add_button_text);
+	$("#empty_msg").append(empty_list_message);
+	
 
