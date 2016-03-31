072d37da3c2ba876b4222dc6841d470ce04d5791
cygnet-ndpatel <cygnet.ndpatel@gmail.com>
2016-03-31 17:54:30 +0530

    index file change

--- Project-Source/js/app.js
+++ Project-Source/js/app.js
@@ -1,9 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('oTask', ['ui.router'])
-    
-		.controller('ApplicationController', ApplicationController);
+    angular.module('oTask', ['ui.router']).controller('ApplicationController', ApplicationController);
 
     ApplicationController.$inject = ['$scope', '$location', '$rootScope'];
 
@@ -11,24 +9,7 @@
         $scope.$on('$stateChangeSuccess', onStateChangeSuccess);
 
         function onStateChangeSuccess(event, toState, toParams, fromState, fromParams) {
-		}
-
-		/*--- onClick full content ---*/
-		$scope.divalert = function($event){
-			var currentItem = $event.currentTarget;	
-			if($(currentItem).find('.ng-binding').text().length > 15 ){
-				$(currentItem).toggleClass('active');
-				$(currentItem).parent().toggleClass('active');
-			}
-		}	
-
-		/*--- onClick overlay ---*/
-		$scope.divoverlay = function($event){
-			var currentItem = $event.currentTarget;
-			$(currentItem).parents('.task-wrapper').toggleClass('activateDargandDrop');
-			$('#clear_list_button').text("Done");
-			$( "#sortable" ).sortable();
-		}		
-	
+	}	
     }
+
 })();
--- Project-Source/js/static-html-content.js
+++ Project-Source/js/static-html-content.js
@@ -3,7 +3,7 @@
 //content variables | TEMPLATE: var a = "";
 	var app_title_text = "oTask";
 	var add_button_text = "Add";
-	var empty_list_message_text = "Your list is empty. Add something new!";
+	var empty_list_message_text = "Your list is empty. Add something new! <br> or <a href='#'>LogIn first</a>";
 	var clear_list_text = "Clear List";
 	var about_otask_text = "About oTask";
 	//var clear_list_text = "Clear List"; //need to work it out still
@@ -14,3 +14,5 @@
 	$("#empty_msg").append(empty_list_message_text);
 	$("#clear_list_button").append(clear_list_text);
 	$("#about_otask_button").append(about_otask_text);
+	
+ 
